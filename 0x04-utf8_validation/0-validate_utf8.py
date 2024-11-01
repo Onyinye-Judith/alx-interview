bd8f889a72d1eba3da09372fae015e6ccc5a7016
“Onyinye-Judith” <onyinyejudith55@yahoo.com>
2024-11-01 15:58:16 +0100

    The-interview

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,33 +1,50 @@
 #!/usr/bin/python3
-"""UTF-8 Validation"""
+"""
+This module contains a function that validates UTF-8 encoding.
+"""
 
-def get_leading_set_bits(num):
-        """returns the number of leading set bits (1)"""
-            set_bits = 0
-                helper = 1 << 7
-                    while helper & num:
-                                set_bits += 1
-                                        helper = helper >> 1
-                                                return set_bits
-                                                def validUTF8(data):
-                                                            """determines if a given data set represents a valid UTF-8 encoding"""
-                                                                    bits_count = 0
-                                                                            for i in range(len(data)):
-                                                                                            if bits_count == 0:
-                                                                                                                bits_count = get_leading_set_bits(data[i])
-                                                                                                                                '''1-byte (format: 0xxxxxxx)'''
-                                                                                                                                                if bits_count == 0:
-                                                                                                                                                                        continue
-                                                                                                                                                                                    '''a character in UTF-8 can be 1 to 4 bytes long'''
-                                                                                                                                                                                                    if bits_count == 1 or bits_count > 4:
-                                                                                                                                                                                                                            return False
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
 
-                                                                                                                                                                                                                                        else:
+                Parameters:
+                    data (List[int]): A list of integers representing bytes of data.
 
-                                                                                                                                                                                                                                                                '''checks if current byte has format 10xxxxxx'''
-                                                                                                                                                                                                                                                                                    if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
-                                                                                                                                                                                                                                                                                                                return False
-                                                                                                                                                                                                                                                                                                                                bits_count -= 1
-                                                                                                                                                                                                                                                                                                                                                  return bits_count == 0
+                        Returns:
+                            bool: True if data is a valid UTF-8 encoding, else False.
+                                """
+                                    # Number of continuation bytes needed
+                                        num_bytes = 0
 
-                     
+                                            # Masks for leading bits in different byte patterns
+                                                byte1_mask = 0b10000000  # 128, to check leading 1 or 0
+                                                    byte2_mask = 0b11000000  # 192, to check '10' in continuation bytes
+                                                        byte3_mask = 0b11100000
+                                                            byte4_mask = 0b11110000
+                                                                byte5_mask = 0b11111000
+
+                                                                    for byte in data:
+                                                                                # Mask the byte to keep only the least significant 8 bits
+                                                                                        byte = byte & 0xFF
+
+                                                                                                if num_bytes == 0:
+                                                                                                                # Determine the number of bytes in the UTF-8 character
+                                                                                                                            if (byte & byte1_mask) == 0:
+                                                                                                                                                continue  # 1-byte character
+                                                                                                                                                        elif (byte & byte3_mask) == 0b11000000:
+                                                                                                                                                                            num_bytes = 1  # 2-byte character
+                                                                                                                                                                                        elif (byte & byte4_mask) == 0b11100000:
+                                                                                                                                                                                                            num_bytes = 2  # 3-byte character
+                                                                                                                                                                                                                        elif (byte & byte5_mask) == 0b11110000:
+                                                                                                                                                                                                                                            num_bytes = 3  # 4-byte character
+                                                                                                                                                                                                                                                        else:
+                                                                                                                                                                                                                                                                            return False  # Invalid leading byte
+                                                                                                                                                                                                                                                                                else:
+                                                                                                                                                                                                                                                                                                # Check if byte is a continuation byte (starts with '10')
+                                                                                                                                                                                                                                                                                                            if (byte & byte2_mask) != 0b10000000:
+                                                                                                                                                                                                                                                                                                                                return False
+                                                                                                                                                                                                                                                                                                                                        num_bytes -= 1
+
+                                                                                                                                                                                                                                                                                                                                            # All bytes should be accounted for
+                                                                                                                                                                                                                                                                                                                                                return num_bytes == 0
+
