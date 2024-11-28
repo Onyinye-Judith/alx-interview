e8c0115b6829cdc08fd8f06bda9d78f0f54fc8d0
Onyinye-Judith <onyinyejudith55@yahoo.com>
2024-11-28 15:33:50 +0100

    Making_Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,18 +6,18 @@ meet a given amount total
 
 
 def makeChange(coins, total):
-        """ fewest number of coins needed to meet total """
-            if total <= 0:
-                        return 0
-                        # sort the coins in descending order
-                            coins.sort(reverse=True)
-                                change = 0
-                                    for coin in coins:
-                                                if total <= 0:
-                                                                break
-                                                                    temp = total // coin
-                                                                            change += temp
-                                                                                    total -= (temp * coin)
+    """ fewest number of coins needed to meet total """
+    if total <= 0:
+        return 0
+    # sort the coins in descending order
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
                                                                                         if total != 0:
                                                                                                     return -1
                                                                                                     return change
