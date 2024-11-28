bcf7c346aceea058bd817dec4bfc9a8119be7651
Onyinye-Judith <onyinyejudith55@yahoo.com>
2024-11-28 18:29:32 +0100

    Making_Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
-
 def makeChange(coins, total):
-    min_coins = [total + 1] * (total + 1)
-    min_coins[0] = 0
-    for amount in range(1, total + 1):
-        for coin in coins:
-            if coin <= amount:
-                min_coins[amount] = min(min_coins[amount], min_coins[amount - coin] + 1)
-                return -1 if min_coins[total] > total else min_coins[total]
+        min_coins = [total + 1] * (total + 1)
+            min_coins[0] = 0
+
+                for amount in range(1, total + 1):
+                            for coin in coins:
+                                            if coin <= amount:
+                                                                min_coins[amount] = min(min_coins[amount], min_coins[amount - coin] + 1)
+
+                                                                    return -1 if min_coins[total] > total else min_coins[total]
