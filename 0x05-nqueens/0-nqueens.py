e38fe43bef3295a9332b3d26f2539e26673f8e2c
“Onyinye-Judith” <onyinyejudith55@yahoo.com>
2025-01-27 00:39:49 +0000

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,38 +1,61 @@
 #!/usr/bin/python3
-"""
-Method to determine if given data represents valid UTF-8 encoding
-Prototype: def validUTF8(data)
-Returns True if data is valid UTF-8 encoding, else return False
-Dataset can contain multiple characters
-Data will represent a list of integers
-"""
+"""N Queens placement on NxN chessboard"""
 
 
-def validUTF8(data):
-    """
-    Prototype: def validUTF8(data)
-    Returns True if data is valid UTF-8 encoding
-    else return False
-    """
-    count = 0
+import sys
 
-    for bit in data:
-        binary = bin(bit).replace('0b', '').rjust(8, '0')[-8:]
-        if count == 0:
-            if binary.startswith('110'):
-                count = 1
-            if binary.startswith('1110'):
-                count = 2
-            if binary.startswith('11110'):
-                count = 3
-            if binary.startswith('10'):
-                return False
-        else:
-            if not binary.startswith('10'):
-                return False
-            count -= 1
 
-    if count != 0:
-        return False
+def generate_solutions(row, column):
+    solution = [[]]
+    for queen in range(row):
+        solution = place_queen(queen, column, solution)
+    return solution
 
-    return True
+
+def place_queen(queen, column, prev_solution):
+    safe_position = []
+    for array in prev_solution:
+        for x in range(column):
+            if is_safe(queen, x, array):
+                safe_position.append(array + [x])
+    return safe_position
+
+
+def is_safe(q, x, array):
+    if x in array:
+        return (False)
+    else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
+
+
+def init():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit():
+        n = int(sys.argv[1])
+    else:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return (n)
+
+
+def n_queens():
+
+    n = init()
+    # generate all solutions
+    solutions = generate_solutions(n, n)
+    # print solutions
+    for array in solutions:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
+
+
+if __name__ == '__main__':
+    n_queens()
